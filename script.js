259611321814f192ffbbc699a267efd528b89863
Cino56 <135582639+Cino56@users.noreply.github.com>
2023-12-04 21:11:44 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,43 @@
+//  // всплываание ококшка
+
+// let string = 'alloha' // строка
+// // let - неявное обьявление переменных
+// // string - имя переменной
+// // = - присвоение значение переменной
+// // 'alloha' - значение переменной
+// alert(string)
+
+// let integer = 10 // целочисл. тип данных
+// console.log(integer) // вывод текста в консоль сайта
+
+// let float = 10.1 // с плав. точкой
+// let boolean = true // = false
+
+
+
+
+
+// let user_number = prompt('Введите число для возведения в квадрат: ')
+
+// // prompt -  для ввода во всплывающую строчку
+
+// user_number = user_number / 2
+
+// alert(user_number)
+
+
+
+// let num_first = Number(prompt('Введите первое число: '))
+// let num_second = Number(prompt('Введите второе число: '))
+
+
+// let mid = (num_first + num_second) / 2
+
+// alert(mid)
+
+
+// let side = Number(prompt('Введите сторону: '))
+
+// side **= 2
+
+// alert(side)
